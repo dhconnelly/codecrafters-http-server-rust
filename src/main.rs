d7d95f57d3526f1de0a58618f118d043125636a9
Daniel Connelly <dhconnelly@gmail.com>
2025-01-15 22:29:32 +0100

    handle

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,72 @@
-use std::net::TcpListener;
+use clap::Parser;
+use std::{
+    io,
+    io::Write,
+    net::{TcpListener, TcpStream},
+    time::Duration,
+};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+#[derive(Parser)]
+#[command(version, about)]
+struct Config {
+    #[arg(default_value = "127.0.0.1")]
+    host: String,
+    #[arg(default_value = "4221")]
+    port: u16,
+    #[arg(default_value = "1000")]
+    write_timeout_ms: u64,
+    #[arg(default_value = "1000")]
+    read_timeout_ms: u64,
+}
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+struct Server {
+    config: Config,
+}
+
+struct Connection {
+    stream: TcpStream,
+}
+
+impl Connection {
+    fn new(config: &Config, stream: TcpStream) -> Result<Self, io::Error> {
+        stream.set_write_timeout(Some(Duration::from_millis(config.write_timeout_ms)))?;
+        stream.set_read_timeout(Some(Duration::from_millis(config.read_timeout_ms)))?;
+        return Ok(Self { stream });
+    }
+
+    fn handle(&mut self) {
+        let s = &mut self.stream;
+        if let Err(err) = write!(s, "HTTP/1.1 200 OK\r\n\r\n") {
+            eprintln!("client {}: error: {}", s.peer_addr().unwrap(), err);
         }
     }
+}
+
+impl Server {
+    fn new(config: Config) -> Self {
+        Self { config }
+    }
+
+    fn handle(&self, stream: TcpStream) -> io::Result<()> {
+        Ok(Connection::new(&self.config, stream)?.handle())
+    }
+
+    fn run(&mut self) -> Result<(), io::Error> {
+        let addr = format!("{}:{}", self.config.host, self.config.port);
+        let listener = TcpListener::bind(&addr).unwrap();
+        println!("listening at http://{}", addr);
+        for stream in listener.incoming() {
+            match stream {
+                Ok(stream) => self.handle(stream)?,
+                Err(err) => return Err(err),
+            }
+        }
+        Ok(())
+    }
+}
+
+fn main() {
+    let cli = Config::parse();
+    let mut server = Server::new(cli);
+    server.run().expect("server failed");
 }
